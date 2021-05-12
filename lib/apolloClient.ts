0c2163c8632d37a4f22b4bb8e2ae896a080dcaea
hsmon <hso92.bb@gmail.com>
2021-05-12 13:34:47 +0900

    update

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -11,15 +11,16 @@ import {
 } from '@apollo/client'
 import 'cross-fetch/polyfill'
 
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
-
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 
 const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: 'https://hasura-lesson.hasura.app/v1/graphql',
+      uri: process.env.NEXT_PUBLIC_HASURA_URL,
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
+      },
     }),
     cache: new InMemoryCache(),
   })
